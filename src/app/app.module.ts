89c79f225527b5f90cf795581b8d2f07f98d6004
Mirza <Tanzeel.Mirza@philips.com>
2019-06-10 23:32:49 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { TorsoComponent } from './torso/torso.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TorsoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
